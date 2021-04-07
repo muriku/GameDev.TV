a50c8a34b17b6b2c78e0f20ba95403fc8dac503c
muriku <muriku@gmail.com>
2021-04-07 10:21:18 +0300

    Inicial commit

---
+++ TrippleX.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+     std::cout << "Тест" << std::endl;
+    // Declare 3 number code
+    const int CodeA = 2;
+    const int CodeB = 4;
+    const int CodeC = 9;
+
+    int CodeSum = CodeA + CodeB + CodeC;
+    int CodeProduct = CodeA * CodeB * CodeC;
+    
+    
+    // Print CodeSum and CodeProduct to the terminal
+    std::cout << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+ 
+    int GuessA, GuessB, GuessC;
+    int GuessSum, GuessProduct;
+    GuessSum = GuessA + GuessB + GuessC;
+    GuessProduct = GuessA * GuessB * GuessC;
+
+    std::cout << "Enter CodeA: "; //<< std::endl;
+    std::cin >> GuessA;
+    std::cout << "Enter CodeB: "; //<< std::endl;
+    std::cin >> GuessB;
+    std::cout << "Enter CodeC: "; //<< std::endl;
+    std::cin >> GuessC;
+    std::cout << std::endl;
+    std::cout << "You entered:" << GuessA << GuessB << GuessC << std::endl;
+    
+    return 0;
+}
