78125d3fec8de8b57b586de7e671e654ce4fb29d
Ilgam Mamedov <muriku83@hotmail.com>
2021-04-07 22:51:52 +0300

    +

--- TrippleX.cpp
+++ TrippleX.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-     std::cout << "rus not working рус" << std::endl;
+    std::cout << "rus not working рус" << std::endl;
     // Declare 3 number code
     int CodeA = 2;
     int CodeB = 4;
@@ -10,17 +10,15 @@ int main()
 
     int CodeSum = CodeA + CodeB + CodeC;
     int CodeProduct = CodeA * CodeB * CodeC;
-    
-    
+
     // Print CodeSum and CodeProduct to the terminal
     std::cout << std::endl;
     std::cout << "+ There are 3 numbers in the code" << std::endl;
     std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
     std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
- 
+
     int GuessA, GuessB, GuessC;
     int GuessSum, GuessProduct;
-    
 
     std::cout << "Enter CodeA: "; //<< std::endl;
     std::cin >> GuessA;
@@ -36,6 +34,6 @@ int main()
     std::cout << "You entered:" << GuessA << GuessB << GuessC << std::endl;
     std::cout << "GuessSum:" << GuessSum << std::endl;
     std::cout << "GuessProduct:" << GuessProduct << std::endl;
-    
+
     return 0;
 }
